d4282f152f2838e41c6ea33da1dbb5efa3e3a498
William Robinson <airbaggins@users.sourceforge.net>
2007-10-18 14:33:36 +0100

    Added some small "fixes" to threading test (basically it still doesn't really work though).

--- examples/mtbench.lisp
+++ examples/mtbench.lisp
@@ -50,14 +50,31 @@ Results:
 
 (format t "Number of CPUs: ~d~%~%" (glfw:get-number-of-processors))
 
-(defparameter *done-mutex* (glfw:create-mutex))
-(defparameter *thread-done* (glfw:create-cond))
-(defparameter *goto-a* (make-instance 'thread-signal))
-(defparameter *goto-b* (make-instance 'thread-signal))
+(defparameter *done-mutex* nil)
+(defparameter *thread-done* nil)
+(defparameter *goto-a* nil)
+(defparameter *goto-b* nil)
 (defparameter *goto-a-count* 0)
 (defparameter *goto-b-count* 0)
 (defparameter *done-count* 0)
 (defparameter *max-count* 10000)
+
+(defun setup ()
+  (declare (optimize (debug 3) (safety 3) (speed 0) (compilation-speed 0)))
+  (setf *done-mutex* (glfw:create-mutex)
+	*thread-done* (glfw:create-cond)
+	*goto-a* (make-instance 'thread-signal)
+	*goto-b* (make-instance 'thread-signal)
+	*goto-a-count* 0
+	*goto-b-count* 0
+	*done-count* 0))
+
+(defun teardown ()
+  (declare (optimize (debug 3) (safety 3) (speed 0) (compilation-speed 0)))
+  (glfw:destroy-mutex *done-mutex*)
+  (glfw:destroy-cond *thread-done*)
+  (kill-signal *goto-a*)
+  (kill-signal *goto-b*))
 
 
 (defmacro make-thread-callback (name signal-var other-signal-var count-var)
@@ -77,34 +94,30 @@ Results:
 (make-thread-callback thread-b-fun *goto-b* *goto-a* *goto-b-count*)
 
 (defun test-1 ()
-  ;; (declare (optimize (debug 3)))
-  (let ((thread-a (glfw:create-thread (cffi:callback thread-a-fun) (cffi:null-pointer)))
-	(thread-b (glfw:create-thread (cffi:callback thread-b-fun) (cffi:null-pointer))))
+  (declare (optimize (debug 3) (safety 3) (speed 0) (compilation-speed 0)))
+  (sb-ext::without-gcing 
+    (let ((thread-a (glfw:create-thread (cffi:callback thread-a-fun) (cffi:null-pointer)))
+	  (thread-b (glfw:create-thread (cffi:callback thread-b-fun) (cffi:null-pointer))))
   
-    (when (or (minusp thread-a) (minusp thread-b))
-      (format t "One of the threads failed~%")
-      (glfw:with-lock-mutex *done-mutex*
-	(setf *done-count* 2)))
+      (when (or (minusp thread-a) (minusp thread-b))
+	(format t "One of the threads failed~%")
+	(glfw:with-lock-mutex *done-mutex*
+	  (setf *done-count* 2)))
 
-    (let ((t1 (glfw:get-time)))
-      (glfw:with-lock-mutex *done-mutex*
-	(loop until (= *done-count* 2)
-	   do (glfw:wait-cond *thread-done* *done-mutex* glfw:+infinity+)))
-      (let* ((t2 (glfw:get-time))
-	     (csps (/ (+ *goto-a-count* *goto-b-count*)
-		      (- t2 t1))))
-	(format t "Test 1: ~,0f context switches / second (~,3f us/switch)~%" csps (/ 1000000 csps))))
+      (let ((t1 (glfw:get-time)))
+	(glfw:with-lock-mutex *done-mutex*
+	  (loop until (= *done-count* 2)
+	     do (glfw:wait-cond *thread-done* *done-mutex* glfw:+infinity+)))
+	(let* ((t2 (glfw:get-time))
+	       (csps (/ (+ *goto-a-count* *goto-b-count*)
+			(- t2 t1))))
+	  (format t "Test 1: ~,0f context switches / second (~,3f us/switch)~%" csps (/ 1000000 csps))))
 
-    (format t "waiting for thread a to finish completely~%")
-    (glfw:wait-thread thread-a glfw:+wait+)
-    (format t "waiting for thread b to finish completely~%")
-    (glfw:wait-thread thread-b glfw:+wait+)
-    (format t "finished waiting~%"))
-
-  (glfw:destroy-mutex *done-mutex*)
-  (glfw:destroy-cond *thread-done*)
-  (kill-signal *goto-a*)
-  (kill-signal *goto-b*))
+      (format t "waiting for thread a to finish completely~%")
+      (glfw:wait-thread thread-a glfw:+wait+)
+      (format t "waiting for thread b to finish completely~%")
+      (glfw:wait-thread thread-b glfw:+wait+)
+      (format t "finished waiting~%"))))
 
 (defun test-2 ()
   (let ((t1 (glfw:get-time))
@@ -120,6 +133,12 @@ Results:
 	    (/ (* 1000.0 (- (glfw:get-time) t1))
 	       count))))
 
-(test-1)
+
+(unwind-protect
+     (progn
+       (setup)
+       (test-1))
+  (teardown))
+
 (test-2)
 (glfw:terminate)
