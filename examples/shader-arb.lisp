bbeeb895144b7bd78e024f1a152c26c3756ffc2b
William Robinson <airbaggins@users.sourceforge.net>
2007-10-21 16:53:23 +0100

    Added example for ARB shaders, so you don't have to REQUIRE opengl 2.0.

--- examples/shader-arb.lisp
+++ examples/shader-arb.lisp
@@ -1,15 +1,18 @@
 (require '#:asdf)
-(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_2_0)
+(asdf:oos 'asdf:load-op '#:cl-glfw-opengl-version_1_1)
 
 (defparameter *shader-program* nil)
 (defparameter *uniform-time* nil)
 
-(glfw:do-window ("An OpenGL 2.0 Shader Example")
+(glfw:do-window ("An ARB Extension Shader Example")
     ((gl:with-setup-projection
        (glu:perspective 45.0d0 (/ 4.0d0 3.0d0) 0.1d0 50.0d0))
-     (setf *shader-program*
-	   (gl:make-program
-	    (gl:make-shader gl:+vertex-shader+ "
+     (when (and (gl-ext:load-extension "ARB_shader_objects")
+		(gl-ext:load-extension "ARB_vertex_shader")
+		(gl-ext:load-extension "ARB_fragment_shader"))
+       (setf *shader-program*
+	     (gl:make-program-arb
+	      (gl:make-shader-arb gl:+vertex-shader-arb+ "
 varying vec3 colour;
 void main()
 {
@@ -17,7 +20,7 @@ void main()
   gl_Position = ftransform();
 }
 ")
-	    (gl:make-shader gl:+fragment-shader+ "
+	      (gl:make-shader-arb gl:+fragment-shader-arb+ "
 uniform float time;
 varying vec3 colour;
 const float pi2=2.0*3.14159265;
@@ -28,15 +31,15 @@ void main()
 	              pow(sin(colour.b*pi2*4.0+mod(time*8.0,pi2)),2.0),
 		      1.0);
 }
-")))
-     (gl:use-program *shader-program*)
-     (setf *uniform-time* (gl:get-uniform-location *shader-program* "time")))
+"))))
+     (gl:use-program-object-arb *shader-program*)
+     (setf *uniform-time* (gl:get-uniform-location-arb *shader-program* "time")))
   (gl:clear gl:+color-buffer-bit+)
   (gl:load-identity)
   (gl:translate-f 0.0 0.0 -5.0)
   (gl:rotate-d (* 10.0d0 (glfw:get-time)) 1d0 1d0 0d0)
   (gl:rotate-d (* 90.0d0 (glfw:get-time)) 0d0 0d0 1d0)
-  (gl:uniform-1f *uniform-time* (coerce (glfw:get-time) 'single-float))
+  (gl:uniform-1f-arb *uniform-time* (coerce (glfw:get-time) 'single-float))
   (gl:with-begin gl:+triangles+
     (gl:color-3f 1.0 0.0 0.0) (gl:vertex-3f  1.0  0.0 0.0)
     (gl:color-3f 0.0 1.0 0.0) (gl:vertex-3f -1.0  1.0 0.0)
