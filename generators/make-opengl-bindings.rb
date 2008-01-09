7320c12d0e287a0d04f65c52c86265a8a53adb67
William Robinson <airbaggins@users.sourceforge.net>
2008-01-09 12:37:39 +0000

    Small usability improvement for main generator interface: --help message, auto-fetch.

--- generators/make-opengl-bindings.rb
+++ generators/make-opengl-bindings.rb
@@ -12,13 +12,26 @@ def check_ok
   end
 end
 
+def fetch_specs
+  puts `wget -P #{BASE}/src -N http://www.opengl.org/registry/api/gl.tm http://www.opengl.org/registry/api/gl.spec  http://www.opengl.org/registry/api/enum.spec  http://www.opengl.org/registry/api/enumext.spec`
+  check_ok
+end
+
 while arg=ARGV.shift
   case arg
   when '--fetch-specs'
-    puts `wget -P #{BASE}/src -c http://www.opengl.org/registry/api/gl.tm http://www.opengl.org/registry/api/gl.spec  http://www.opengl.org/registry/api/enum.spec  http://www.opengl.org/registry/api/enumext.spec`
-    check_ok
+    fetch_specs
+  when '--help'
+    puts "Re-generates OpenGL bindings from specifications."
+    puts "Use --fetch-specs to force re-download of spec files from http://www.opengl.org/registry"
+    exit
   end
 end
+
+fetch_specs unless File::exists?("#{BASE}/src/gl.tm") and
+    File::exists?("#{BASE}/src/gl.spec") and
+    File::exists?("#{BASE}/src/enum.spec") and
+    File::exists?("#{BASE}/src/enumext.spec")
 
 puts `ruby #{BASE}/generators/sexpize-gl-spec.rb`
 check_ok
