5960591124c1ace3bdaac118e7395e21e1e14289
Bill Robinson <airbaggins@gmail.com>
2010-10-29 08:33:56 +0100

    Fixed cffi library specification syntax

--- lib/opengl-library.lisp
+++ lib/opengl-library.lisp
@@ -4,6 +4,6 @@
   (:windows (:or "opengl32" "opengl32.dll" "opengl"))
   (:darwin (:framework "OpenGL"))
   (:unix (:or "libGL" "libGL.so" "libGL.so.2" "libGL.so.1.2" "libGL.so.1"))
-  (t "libGL" "GL"))
+  (t (:or "libGL" "GL")))
 
 (cffi:use-foreign-library opengl)
